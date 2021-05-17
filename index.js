0c859fed4a1d38c5f1defae602305077ce199448
凱恩Kane <33802653+Gary50613@users.noreply.github.com>
2021-05-18 00:08:31 +0800

    改為初始化instance

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ require('dotenv').config();
 const Discord = require('./Discord');
 const RSS = require('./RSS');
 
-Discord.awake(process.env.BOT_TOKEN, process.env.CHANNEL_ID)
-    .then(() => RSS.awake());
+new Discord().awake(process.env.BOT_TOKEN, process.env.CHANNEL_ID)
+    .then(() => new RSS().awake());
