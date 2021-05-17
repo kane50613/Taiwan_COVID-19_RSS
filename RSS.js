128aa4908c9e402bbf8eb03364de97920addc4fd
wolf-yuan-6115 <h1030035@gmail.com>
2021-05-17 23:43:18 +0800

    Rewrite code - Beta(Need to test)

--- RSS.js
+++ RSS.js
@@ -1,21 +1,23 @@
-const RssFeedEmitter = require('rss-feed-emitter')
-const Discord = require('./Discord')
+const RssFeedEmitter = require('rss-feed-emitter');
+const Discord = require('./Discord');
 
 class RSS {
-    static feeder = new RssFeedEmitter()
+    constructor() {
+        this.feeder = new RssFeedEmitter();
+    }
 
-    static awake = () => {
+    awake() {
         this.feeder.add({
             url: 'https://www.cdc.gov.tw/RSS/RssXml/Hh094B49-DRwe2RR4eFfrQ?type=1',
             refresh: 10000
-        })
+        });
 
         this.feeder.on('new-item', async item => {
             if(Date.parse(item['a10:updated']['#']) < Date.now())
-                return console.log(`[INFO] 過去的訊息 - ${item?.title}`)
-            Discord.createMessage(item)
-        })
+                return console.log(`[INFO] 過去的訊息 - ${item.title}`);
+            Discord.createMessage(item);
+        });
     }
 }
 
-module.exports = RSS
+module.exports = RSS;
--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-require("dotenv").config()
+require('dotenv').config();
 
-const Discord = require('./Discord')
-const RSS = require('./RSS')
+const Discord = require('./Discord');
+const RSS = require('./RSS');
 
 Discord.awake(process.env.BOT_TOKEN, process.env.CHANNEL_ID)
-.then(() => RSS.awake())
-
+    .then(() => RSS.awake());
