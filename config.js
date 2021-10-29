385d927a579a2980e73634846dad632c33778ebf
Kayxue <s0990006@gmail.com>
2021-10-29 23:30:59 +0800

    Modified to ES Module Version

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 export default {
-  "discord": {
-    "token": "Nzg0MDA2NTA5NDQ5MzE0MzA0.X8jA9Q.aHPJTIvVqJuEWZ5yZ47BcQ5zIAA",
-    "channel": "647650604684148748"
+  discord: {
+    token: "",
+    channel: ""
   },
-  "rss": {
-    "targets": [
+  rss: {
+    targets: [
       "https://www.cdc.gov.tw/RSS/RssXml/Hh094B49-DRwe2RR4eFfrQ?type=1"
     ],
-    "interval": 5000
+    interval: 5000
   }
 }
